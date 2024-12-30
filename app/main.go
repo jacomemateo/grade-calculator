fd9b34c56f7eff89e69f55004131eab6266879e1
mateo <mateo.jacome@hotmail.com>
2024-12-29 21:11:31 -0500

    added some stuff

--- app/main.go
+++ app/main.go
@@ -1,37 +1,32 @@
 package main
 
 import (
-	"database/sql"
+	"fmt"
 	"grade-calculator/views"
 	"log"
 	"net/http"
 
 	"github.com/a-h/templ"
 	"github.com/gin-gonic/gin"
-	_ "github.com/mattn/go-sqlite3"
+	"gorm.io/driver/sqlite"
+	"gorm.io/gorm"
 )
 
-func initDB() (*sql.DB, error) {
-	db, err := sql.Open("sqlite3", "./db/data.db")
+type Grade struct {
+	GradeValue int32
+	GradeName  string
+}
+
+func initDB() (*gorm.DB, error) {
+	db, err := gorm.Open(sqlite.Open("db/test.db"), &gorm.Config{})
 
 	if err != nil {
 		return nil, err
 	}
 
-	query := `
-	CREATE TABLE IF NOT EXISTS users (
-		id INTEGER PRIMARY KEY AUTOINCREMENT,
-		name TEXT NOT NULL,
-		email TEXT NOT NULL UNIQUE
-	);
-	`
+	db.AutoMigrate(&Grade{})
 
-	_, err = db.Exec(query)
-	if err != nil {
-		return nil, err
-	}
-
-	return db, nil
+	return db, err
 }
 
 func render(c *gin.Context, status int, template templ.Component) error {
@@ -43,15 +38,40 @@ func main() {
 	// Initialize SQLite database
 	db, err := initDB()
 	if err != nil {
-		log.Fatal("Error initializing the database:", err)
+		log.Fatal("Error initalizing the database: ", err)
 	}
-	defer db.Close()
 
+	// Create http server
 	r := gin.Default()
 	r.Static("/static", "./static")
 
 	r.GET("/", func(c *gin.Context) {
-		render(c, http.StatusOK, views.MakeHome("Mateo's Cool", "Felipe"))
+		render(c, http.StatusOK, views.MakeHome("Grade Calculator"))
+	})
+
+	r.POST("/submit", func(c *gin.Context) {
+		// Parse form data
+		gradeName := c.PostForm("gradeName")
+		gradeValue := c.PostForm("gradeValue")
+
+		// Convert gradeValue to integer
+		var gradeValueInt int32
+		_, err := fmt.Sscanf(gradeValue, "%d", &gradeValueInt)
+		if err != nil {
+			c.String(http.StatusBadRequest, "Invalid grade value")
+			return
+		}
+
+		// Create a new Grade and save it to the database
+		grade := Grade{
+			GradeName:  gradeName,
+			GradeValue: gradeValueInt,
+		}
+
+		if result := db.Create(&grade); result.Error != nil {
+			c.String(http.StatusInternalServerError, "Failed to save grade")
+			return
+		}
 	})
 
 	r.Run(":9090")
--- go.mod
+++ go.mod
@@ -55,5 +55,6 @@ require (
 	golang.org/x/tools v0.28.0 // indirect
 	google.golang.org/protobuf v1.36.1 // indirect
 	gopkg.in/yaml.v3 v3.0.1 // indirect
+	gorm.io/driver/sqlite v1.5.7 // indirect
 	gorm.io/gorm v1.25.12 // indirect
 )
